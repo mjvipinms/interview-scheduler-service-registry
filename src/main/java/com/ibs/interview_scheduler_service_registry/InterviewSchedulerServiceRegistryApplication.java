8c0fb7182f4447426e7f950648c5d51a003f777e
vipin <mjvipinms@gmail.com>
2025-10-14 12:56:46 +0530

    Initial commit

---
+++ src/main/java/com/ibs/interview_scheduler_service_registry/InterviewSchedulerServiceRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.ibs.interview_scheduler_service_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class InterviewSchedulerServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewSchedulerServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibs/interview_scheduler_service_registry/InterviewSchedulerServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibs.interview_scheduler_service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewSchedulerServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
